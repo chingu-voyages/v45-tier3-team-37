e1eacd3ab6de7f5bf67b9759d58300d1b9616bad
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-08-17 16:02:13 +0100

    feat: render Navbar component

--- bookfinder/src/app/(root)/page.tsx
+++ bookfinder/src/app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
     <main className=" flex bg-inherit">
-      <h1>BookApp</h1>
+      <Navbar />
     </main>
   );
 }
