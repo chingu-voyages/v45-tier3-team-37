f854896987848341182f72aa41bdec6d1e8bb4d3
frugalcodes <samueligwe63@gmail.com>
2023-08-28 20:07:38 +0100

    editted options

--- bookfinder/src/components/SearchInput.tsx
+++ bookfinder/src/components/SearchInput.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 
 const SearchInput = () => {
-
   return (
     <div className="left-0 top-0 w-full shadow-md">
     <div className="w-full bg-white px-4 py-3 sm:px-8 sm:py-6">
       <div className="flex flex-col sm:flex-row items-center">
         <select className="w-full sm:w-32 bg-white border border-gray-300 px-3 py-2 rounded-md mb-2 sm:mb-0 sm:mr-2 focus:outline-none">
+          <option value="">ALL</option>
+          <option value="">AUTHOR</option>
           <option value="">TITLE</option>
+          <option value="">PUBLISHER</option>
+        
           {/* Categories options */}
         </select>
         <input
