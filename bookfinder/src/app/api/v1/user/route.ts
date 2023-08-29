a14a858eae9db0202cd31a85ce7f9758018bd7fb
Jair Oliveira (Ceifeirocv) <jair.almeida.oliveira@gmail.com>
2023-08-29 11:25:24 -0100

    feat: will check user and save every time user login

--- bookfinder/src/app/api/v1/user/route.ts
+++ bookfinder/src/app/api/v1/user/route.ts
@@ -4,8 +4,11 @@ import { NextResponse } from "next/server";
 import { currentUser } from "@clerk/nextjs";
 import type { User as ClerkUser } from "@clerk/nextjs/api";
 
-export async function POST(request: Request) {
+export async function GET(request: Request) {
   const userClerk: ClerkUser | null = await currentUser();
+  if (!userClerk) {
+    NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
   try {
     await connectMongoDB();
 
@@ -13,11 +16,11 @@ export async function POST(request: Request) {
     if (!user) {
       user = await User.create({
         _id: userClerk?.id,
-        email: userClerk?.emailAddresses[0],
+        email: userClerk?.primaryEmailAddressId,
       });
     }
 
-    return NextResponse.json(user);
+    return NextResponse.redirect(new URL("/", await request.url));
   } catch (error) {
     if (error instanceof Error) {
       console.error(error.message); // Known error type
