b89c88a55b40b725ec7691154dd26411139403b1
Jair Oliveira (Ceifeirocv) <jair.almeida.oliveira@gmail.com>
2023-08-30 09:30:08 -0100

    chore: add favorite schema

--- bookfinder/src/models/user.ts
+++ bookfinder/src/models/user.ts
@@ -11,10 +11,23 @@ const userSchema = new mongoose.Schema(
           cover: { type: String },
           title: { type: String, required: true },
           description: { type: String },
+          price: { type: Number },
         },
       ],
       default: [],
     },
+  },
+  { timestamps: true },
+);
+
+const favoriteSchema = new mongoose.Schema(
+  {
+    identifier: { type: String, required: true },
+    userId: { type: String, required: true },
+    cover: { type: String },
+    title: { type: String, required: true },
+    description: { type: String },
+    price: { type: Number },
   },
   { timestamps: true },
 );
