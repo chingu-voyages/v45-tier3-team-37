15c9863a2c90532f88a384231b5447ac9ed7be18
tugba <78135546+yildiztugba@users.noreply.github.com>
2023-09-13 15:07:02 +0200

    fetch fails

--- bookfinder/src/components/EBook.tsx
+++ bookfinder/src/components/EBook.tsx
@@ -92,16 +92,15 @@ let books:any= []
         <Slider {...settings}>
           {
         EBookList.length === 0 ? <>
-		<div
-  className="inline-block h-4 w-4 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-  role="status">
-  <span
-    className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-    >Loading...</span
-  >
-</div>
-</>:(
-        
+        <div
+          className="inline-block h-4 w-4 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status">
+        <span
+          className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+          >Loading...</span>
+        </div>
+        </>:(
+            
               EBookList?.map((book:any) => (
               <div className="p-6" key={book.rank}>
                 <BookCard 
@@ -112,13 +111,12 @@ let books:any= []
                       publisher={book.volumeInfo.publisher}
                       description={book.volumeInfo.description}
                       identifier={""}
-                      date={book.date}
+                      date={book.volumeInfo.publishedDate}
                       key={book.id}
                       noInfo = {true}
                       />
-                  </div>)
-              )
-)}
+                  </div>))
+          )}
                 
         </Slider>
         </section>
