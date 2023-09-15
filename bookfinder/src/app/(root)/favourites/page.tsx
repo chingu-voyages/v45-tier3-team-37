e24a48df6c1dfbdea8c4d7924f539bb02415529f
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-09-15 15:12:16 +0100

    feat: use FavoriteBookCard component to render favorites

--- bookfinder/src/app/(root)/favourites/page.tsx
+++ bookfinder/src/app/(root)/favourites/page.tsx
@@ -2,14 +2,31 @@ import NoFavouritesCard from "@/components/NoFavouritesCard";
 import Image from "next/image";
 import Link from "next/link";
 import { getFavorite, getFavoriteSSC } from "@/utils/fetcher";
+import { BookFavoriteDB } from "@/lib/types";
+import FavoriteBookCard from "@/components/FavoriteBookCard";
 
 const Favourites = async () => {
   const favorites = await getFavoriteSSC();
-  console.log("favorites: ", favorites);
 
   return (
     <div className="flex h-full items-center justify-center py-16">
-      <NoFavouritesCard />
+      {favorites.length === 0 ? (
+        <NoFavouritesCard />
+      ) : (
+        <div className="flex w-28 gap-2">
+          {favorites.map((fav: BookFavoriteDB) => (
+            <FavoriteBookCard
+              id={fav.Id}
+              identifier={fav.identifier}
+              cover={fav.cover}
+              title={fav.title}
+              authors={fav.author}
+              seller={fav.seller}
+              createdAt={fav.createdAt}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
