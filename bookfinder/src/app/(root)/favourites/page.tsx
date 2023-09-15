eaea075e54e18f08d6666207c66ed9e3bce90a1f
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-09-15 16:51:45 +0100

    feat: render style improvements

--- bookfinder/src/app/(root)/favourites/page.tsx
+++ bookfinder/src/app/(root)/favourites/page.tsx
@@ -9,24 +9,29 @@ const Favourites = async () => {
   const favorites = await getFavoriteSSC();
 
   return (
-    <div className="flex h-full items-center justify-center py-16">
-      {favorites.length === 0 ? (
-        <NoFavouritesCard />
-      ) : (
-        <div className="flex w-28 gap-5">
-          {favorites.map((fav: BookFavoriteDB) => (
-            <FavoriteBookCard
-              id={fav.Id}
-              identifier={fav.identifier}
-              cover={fav.cover}
-              title={fav.title}
-              authors={fav.author}
-              seller={fav.seller}
-              createdAt={fav.createdAt}
-            />
-          ))}
-        </div>
-      )}
+    <div className="flex w-full justify-center">
+      <div className="max-w-7xl">
+        {favorites.length === 0 ? (
+          <div className="flex h-full items-center justify-center py-16">
+            <NoFavouritesCard />
+          </div>
+        ) : (
+          <div className="flex flex-wrap gap-5 py-16">
+            {favorites.map((fav: BookFavoriteDB) => (
+              <FavoriteBookCard
+                key={fav.Id}
+                id={fav.Id}
+                identifier={fav.identifier}
+                cover={fav.cover}
+                title={fav.title}
+                author={fav.author}
+                seller={fav.seller}
+                createdAt={fav.createdAt}
+              />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
