017721c77a1973b9852efcdc74514d2240b38598
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-09-15 16:51:24 +0100

    feat: add date formatting and style improvements to FavoriteBookCard

--- bookfinder/src/components/FavoriteBookCard.tsx
+++ bookfinder/src/components/FavoriteBookCard.tsx
@@ -1,13 +1,26 @@
 import { FavoriteSeller } from "@/lib/types";
+import Image from "next/image";
 
 type FavoriteBookDetails = {
   id: string;
   identifier: string;
   cover?: string;
   title: string;
-  authors: string[];
+  author: string[];
   seller: FavoriteSeller[];
   createdAt: string;
+};
+
+const formatDate = (dateCreated: string) => {
+  const inputDate = new Date(dateCreated);
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  const formattedDate = inputDate.toLocaleDateString("en-US", options);
+  return formattedDate;
 };
 
 const FavoriteBookCard = ({
@@ -15,21 +28,20 @@ const FavoriteBookCard = ({
   identifier,
   cover,
   title,
-  authors,
+  author,
   seller,
   createdAt,
 }: FavoriteBookDetails) => {
   return (
-    <div className="flex flex-col items-center gap-3 rounded-md border-2 px-6 py-4 shadow-md">
-      <img src={cover} alt="Book Cover" />
-      <p>Title: {title}</p>
-      <p>
-        Author(s):{" "}
-        {authors.map((author) => (
-          <span>{author}</span>
-        ))}
+    <div className="flex w-96 flex-col items-center gap-3 rounded-md border-2 px-6 py-4 shadow-md">
+      <img className="h-48 w-48 object-contain" src={cover} alt="Book Cover" />
+      <p className="text-lg font-bold">
+        Title: <span>{title}</span>
       </p>
-      <p>Date Added: {createdAt}</p>
+      <p className="w-56 break-normal">
+        Author(s): <span>{author.join(", ")}</span>
+      </p>
+      <p>Date Added: {formatDate(createdAt)}</p>
     </div>
   );
 };
